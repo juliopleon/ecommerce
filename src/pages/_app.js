61ef6630b0b4164204f52eb2901fccb7a18e9756
Julio Leon <juliopena143@icloud.com>
2023-03-30 23:55:54 -0600

    last push for the night

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+import React from 'react';
+
 import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default MyApp
+
+
