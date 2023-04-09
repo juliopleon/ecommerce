948cd9f0f1b4b87f6501942d80a67ced25f2cf31
Julio Leon <juliopena143@icloud.com>
2023-04-08 23:14:22 -0600

    react component update

--- components/Product.jsx
+++ components/Product.jsx
@@ -12,7 +12,7 @@ const Product = ({ product: { image, name, slug, price } }) => {
                         src={urlFor(image && image[0])}
                         width={250}
                         height={250}
-                        className="product-image"
+                        className="product-images"
                     />
                     <p className="product-name">{name}</p>
                     <p className="product-price">${price}</p>
