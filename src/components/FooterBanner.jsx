37b0be6ff0b0a5727c12298ea172dcde1d920ac9
Julio Leon <juliopena143@icloud.com>
2023-04-19 16:45:12 -0600

    footerBanner work

--- src/components/FooterBanner.jsx
+++ src/components/FooterBanner.jsx
@@ -3,9 +3,21 @@ import Link from 'next/link';
 
 import { urlFor } from '@/lib/client';
 
-const FooterBanner = ({ FooterBanner }) => {
+const FooterBanner = ({ footerBanner }) => {
     return (
-        <div>FooterBanner</div>
+        <div className="footer-banner-container">
+            <div className="banner-desc">
+                <div className="left">
+                    <p>{footerBanner.discount}</p>
+                    <h3>{footerBanner.largeText1}</h3>
+                    <h3>{footerBanner.largeText2}</h3>
+                    <p>{footerBanner.saleTime}</p>
+                </div>
+                <div className="right">
+
+                </div>
+            </div>
+        </div>
     )
 }
 
