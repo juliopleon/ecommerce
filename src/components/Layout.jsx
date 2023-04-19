35a5891d28c18d7232fbddacef97d16254e12e5a
Julio Leon <juliopena143@icloud.com>
2023-04-19 17:16:33 -0600

    cart

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
-import Navbar from './Navbar';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
     return (
         <div className="layout">
             <Head>
@@ -11,6 +13,12 @@ const Layout = () => {
             <header>
                 <Navbar />
             </header>
+            <main className="main-container">
+                {children}
+            </main>
+            <footer>
+                <Footer />
+            </footer>
         </div>
     )
 }
