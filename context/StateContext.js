d3a0224a1ea06f2854c7b9c94a2be1aa013640c2
Julio Leon <juliopena143@icloud.com>
2023-06-27 20:01:21 -0600

    cart qty and add to cart done

--- context/StateContext.js
+++ context/StateContext.js
@@ -6,9 +6,9 @@ const Context = createContext();
 
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] = useState();
+    const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState();
+    const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
     const onAdd = (product, quantity) => {
