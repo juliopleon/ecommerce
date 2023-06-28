fbf56b8b568abbfd16d4551a49b24d894ce3a65c
Julio Leon <juliopena143@icloud.com>
2023-06-27 18:50:18 -0600

    state context for cart

---
+++ context/StateContext.js
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { toast } from 'react-hot-toast';
+
+const context = createContext();
+
+
+export const StateContext = ({ children }) => {
+    const [showCart, setShowCart] = useState(false);
+    const [cartItems, setCartItems] = useState();
+    const [totalPrice, setTotalPrice] = useState();
+    const [totalQuantities, setTotalQuantities] = useState();
+    const [qty, setQty] = useState(1);
+
+    return (
+        <Context.Provider>
+            {children}
+        </Context.Provider>
+    )
+
+}
