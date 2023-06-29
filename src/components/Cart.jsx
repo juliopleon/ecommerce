3f53375a7aa8c67fb9a16b930a28ad5e02185110
Julio Leon <juliopena143@icloud.com>
2023-06-28 19:18:51 -0600

    imports for cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,8 @@ import React, { useRef } from 'react';
 import Link from 'next/link';
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti';
-import { Toast } from 'react-hot-toast'; import { useStateContext } from '../../context/StateContext';
+import { Toast } from 'react-hot-toast';
+import { useStateContext } from '../../context/StateContext';
 import { urlFor } from '@/lib/client';
 
 
