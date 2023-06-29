84f1f7963c7f2e390265f8cb3d994bb1c1093723
Julio Leon <juliopena143@icloud.com>
2023-06-28 19:17:48 -0600

    cart work

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import Link from 'next/link';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import { Toast } from 'react-hot-toast'; import { useStateContext } from '../../context/StateContext';
+import { urlFor } from '@/lib/client';
+
+
 
 const Cart = () => {
+    const cartRef = useRef();
+    const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext()
     return (
         <div>Cart</div>
     )
