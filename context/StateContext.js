59a010fe8110b1fbb354f8d8c4ff5fd5b437123f
Julio Leon <juliopena143@icloud.com>
2023-07-24 23:49:53 -0600

    state

--- context/StateContext.js
+++ context/StateContext.js
@@ -41,7 +41,7 @@ export const StateContext = ({ children }) => {
     const toggleCartItemQuantity = (id, value) => {
         foundProduct = cartItems.find((item) => item._id === id);
         index = cartItems.findIndex((product) => product._id === id);
-        const newCartItems = cartItems.splice(index, 1)
+        const newCartItems = cartItems.filter((item) => item._id !== id)
 
         if (value === 'inc') {
             setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }])
